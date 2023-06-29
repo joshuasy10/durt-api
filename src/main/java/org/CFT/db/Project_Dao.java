3f2e175b0870e23684d6fe615bcbc56124196f7f
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-29 14:30:12 +0100

    fixed everything again lol

--- src/main/java/org/CFT/db/Project_Dao.java
+++ src/main/java/org/CFT/db/Project_Dao.java
@@ -10,30 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Project_Dao {
-<<<<<<< HEAD
-//    private DatabaseConnector databaseConnector = new DatabaseConnector();
-//    public List<Project> getAllProjects() throws SQLException {
-//        Connection c = databaseConnector.getConnection();
-//        Statement st = c.createStatement();
-//
-//        ResultSet rs = st.executeQuery("SELECT id, client_id, name, value FROM `projects`");
-//
-//        List<Project> projectList = new ArrayList<>();
-//
-//        while (rs.next()) {
-//            Project project = new Project(
-//                    rs.getInt("id"),
-//                    rs.getInt("client_id"),
-//                    rs.getString("name"),
-//                    rs.getDouble("value")
-//            );
-//
-//            projectList.add(project);
-//        }
-//
-//        return projectList;
-//    }
-=======
     private DatabaseConnector databaseConnector = new DatabaseConnector();
     public List<Project> getAllProjects() throws SQLException {
         Connection c = databaseConnector.getConnection();
@@ -126,5 +102,4 @@ public class Project_Dao {
             st.executeUpdate();
         }
     }
->>>>>>> dfa3910 (oh no)
 }
