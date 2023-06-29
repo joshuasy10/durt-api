de4775938dbf738979c001098950df393044019d
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-29 11:50:13 +0100

    Added Client_Dao

---
+++ src/main/java/org/CFT/db/Client_Dao.java
@@ -0,0 +1,33 @@
+package org.CFT.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client_Dao {
+    DatabaseConnector databaseConnector = new DatabaseConnector();
+    public List<Client> getAllClients() throws SQLException {
+        Connection c = databaseConnector.getConnection();
+        Statement st = c.createStatement();
+
+        ResultSet rs = st.executeQuery("SELECT id, name, address, phone FROM `clients`");
+
+        List<Client> clientList = new ArrayList<>();
+
+        while (rs.next()) {
+            Client client = new Client(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("address"),
+                    rs.getString("phone")
+            );
+
+            clientList.add(client);
+        }
+
+        return clientList;
+    }
+}
